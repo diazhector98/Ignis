29e30085eb833440930c47076ad29f2b6d063591
Hector Diaz <diazhector98@gmai.com>
2019-05-02 20:59:59 -0500

    Add world refactoring

---
+++ src/ignis/Worlds/AlkalineWorld.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ignis.Worlds;
+
+import ignis.Game;
+import ignis.Player;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author hectordiazaceves
+ */
+public class AlkalineWorld extends World {
+
+    public AlkalineWorld(Game g, Player p) {
+        super(g, p, 2);
+    }
+
+    @Override
+    public void tick() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void generateWorld() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void render(Graphics g) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/ignis/Worlds/NonMetalWorld.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ignis.Worlds;
+
+import ignis.Game;
+import ignis.Player;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author hectordiazaceves
+ */
+public class NonMetalWorld extends World{
+
+    public NonMetalWorld(Game g, Player p) {
+        super(g, p, 8);
+    }
+
+    @Override
+    public void tick() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void generateWorld() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void render(Graphics g) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
