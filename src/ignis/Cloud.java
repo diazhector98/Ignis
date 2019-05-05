58c6dfed30399408f995002f0d69dcfee6ea0a90
alfredofloresnt <alfredofloresn.t@gmail.com>
2019-05-05 18:38:30 -0500

    Added worlds

---
+++ src/ignis/Cloud.java
@@ -0,0 +1,20 @@
+/*i+=0.1;
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ignis;
+
+/**
+ *
+ * @author hectordiazaceves
+ */
+public class Cloud extends Enemy{
+    int step=0;
+    public Cloud(Platform p) {
+        super(3, 5, p, 1);
+        
+    }
+    
+    
+}
