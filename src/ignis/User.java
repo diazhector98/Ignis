35f0f6106dc4fac964b71c514071f610ae326b90
Hector Diaz <diazhector98@gmai.com>
2019-05-08 00:08:51 -0500

    Add User class

---
+++ src/ignis/User.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ignis;
+
+/**
+ *
+ * @author hectordiazaceves
+ */
+public class User {
+    
+    int ID;
+    String USERNAME;
+    
+    Database database;
+    
+    public User(int ID, String username){
+     this.ID = ID;
+     this.USERNAME = username;
+     this.database = new Database();
+    }
+    
+    
+    
+}
